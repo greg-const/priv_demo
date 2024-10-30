f72e27a818cc1e407b5e1aff7af9891bf32984c5
greg-const <86847050+greg-const@users.noreply.github.com>
2024-10-30 18:57:16 -0400

    Update Basic.lean

--- NEWPROJ/Basic.lean
+++ NEWPROJ/Basic.lean
@@ -1,1 +1,18 @@
-def hello := "world"
+theorem thm1 : p ∧ q ↔ q ∧ p := by
+  apply Iff.intro
+  · intro hpq
+    apply And.intro
+    exact hpq.right
+    exact hpq.left
+  · intro hqp
+    apply And.intro
+    exact hqp.right
+    exact hqp.left
+
+
+theorem thm2 : ∀ a b c : ℕ, a = b → a = c → c = b := by
+  intros
+  apply Eq.trans
+  apply Eq.symm
+  assumption
+  assumption
